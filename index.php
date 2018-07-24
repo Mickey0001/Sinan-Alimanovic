496a890b2d2ad852d9fcf627dc8dd7ecfc094def
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-24 18:36:13 +0200

    Include header

--- index.php
+++ index.php
@@ -1,46 +1,5 @@
-<!DOCTYPE html>
-<html <?php language_attributes();?>>
-<head>
-  <meta charset="<?php bloginfo('charset');?>">
-  <meta "description" content="<?php bloginfo('description');?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>
-    <?php bloginfo('name');?> |
-    <?php wp_title(); ?>
-    <?php is_front_page() ? bloginfo('description') : wp_title(); ?>
-  </title>
-  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/css/bootstrap.min.css">
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
-</head>
-<body>
-
-    <nav class="navbar navbar-expand-lg">
-        <a class="navbar-brand" href="#">HOME</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button> 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-          </ul>
-          <ul class="navbar-nav my-2 my-lg-0">
-              <li class="nav-item">
-                  <a class="nav-link" href="#">PHOTOS</a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="#">BIOGRAPHY</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">NEWS</a>
-            </li>
-              <li class="nav-item">
-                  <a class="nav-link" href="#">CONTACT</a>
-              </li>
-          </ul>
-        </div>
-      </nav>
-
-      <div class="main-container">
+<?php get_header(); ?>
+<div class="main-container">
   
   <div id="demo" class="carousel slide" data-ride="carousel" data-interval="false">
 
@@ -73,6 +32,8 @@
 
 
 <footer></footer>
+
+<?php wp_footer(); ?>
 
   <script src="js/jquery-3.3.1.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
