be00e0cf232ecbf35d4e090fb9fd97740faec0eb
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-25 22:19:37 +0200

    Page, heade and index

--- header.php
+++ header.php
@@ -1,38 +1,45 @@
 <!DOCTYPE html>
-<html <?php language_attributes();?>>
-<head>
-  <meta charset="<?php bloginfo('charset');?>">
-  <meta "description" content="<?php bloginfo('description');?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>
-    <?php bloginfo('name');?> |
-    <?php wp_title(); ?>
-    <?php is_front_page() ? bloginfo('description') : wp_title(); ?>
-  </title>
-  <link rel="stylesheet" href="<?php bloginfo('template_url');?>/css/bootstrap.min.css">
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
-  <?php wp_head(); ?>
-</head>
-<body>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <title>
+      <?php bloginfo('name'); ?> |
+      <?php is_front_page() ? bloginfo('description') : wp_title(); ?>
+    </title>
+    <!-- Bootstrap core CSS -->
+    <link href="<?php bloginfo('template_url'); ?>/css/bootstrap.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <?php
+           wp_nav_menu( array(
+               'menu'              => 'primary',
+               'theme_location'    => 'primary',
+               'depth'             => 2,
+               'container'         => 'div',
+               'container_class'   => 'collapse navbar-collapse',
+       'container_id'      => 'bs-example-navbar-collapse-1',
+               'menu_class'        => 'nav navbar-nav',
+               'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+               'walker'            => new wp_bootstrap_navwalker())
+           );
+       ?>
+        </nav>
+      </div>
+    </div>
 
-<nav class="navbar navbar-expand-lg" role="navigation">
-  <div class="container">
-	<!-- Brand and toggle get grouped for better mobile display -->
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-		<?php
-		wp_nav_menu( array(
-			'theme_location'    => 'primary',
-			'depth'             => 2,
-			'container'         => 'div',
-			'container_class'   => 'collapse navbar-collapse',
-			'container_id'      => 'bs-example-navbar-collapse-1',
-			'menu_class'        => 'nav navbar-nav',
-			'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-			'walker'            => new WP_Bootstrap_Navwalker(),
-		) );
-		?>
-	</div>
-</nav>
+    <div class="container">
+      <div class="blog-header">
+        <h1 class="blog-title"><?php bloginfo('name'); ?></h1>
+        <p class="lead blog-description"><?php bloginfo('description'); ?></p>
+      </div>
