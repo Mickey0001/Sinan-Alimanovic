08260f849739c44b75609bf535b2f0f2094f9f21
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-25 22:18:48 +0200

    Update footer, functions and front-page

--- functions.php
+++ functions.php
@@ -1,22 +1,69 @@
-<?php 
+<?php
+  // Register Nav Walker class_alias
+  require_once('wp_bootstrap_navwalker.php');
 
-//Register Nav Walker class_alias
-require_once('class-wp-bootstrap-navwalker.php');
+  // Theme Support
+  function wpb_theme_setup(){
+    add_theme_support('post-thumbnails');
 
-//Theme setup
-function sinan_theme_setup()
-{
-  register_nav_menus( array(
-    'primary' => __( 'Primary Menu', 'THEMENAME' ),
-  ) );
+    // Nav Menus
+    register_nav_menus(array(
+      'primary' => __('Primary Menu')
+    ));
+
+    // Post Formats
+    add_theme_support('post-formats', array('aside', 'gallery'));
+  }
+
+  add_action('after_setup_theme','wpb_theme_setup');
+
+// Excerpt Length Control
+function set_excerpt_length(){
+  return 45;
 }
 
-add_action('after_setup_theme', 'sinan_theme_setup');
+add_filter('excerpt_length', 'set_excerpt_length');
 
-//Posts exceprt lenght control
-function set_excerpt_length()
-{
-  return 20;
+// Widget Locations
+function wpb_init_widgets($id){
+  register_sidebar(array(
+    'name'  => 'Sidebar',
+    'id'    => 'sidebar',
+    'before_widget' => '<div class="sidebar-module">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h4>',
+    'after_title'   => '</h4>'
+  ));
+
+  register_sidebar(array(
+    'name'  => 'Box1',
+    'id'    => 'box1',
+    'before_widget' => '<div class="box">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ));
+
+  register_sidebar(array(
+    'name'  => 'Box2',
+    'id'    => 'box2',
+    'before_widget' => '<div class="box">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ));
+
+  register_sidebar(array(
+    'name'  => 'Box3',
+    'id'    => 'box3',
+    'before_widget' => '<div class="box">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ));
 }
 
-?>
+add_action('widgets_init', 'wpb_init_widgets');
+
+// Customizer File
+require get_template_directory(). '/inc/customizer.php';
