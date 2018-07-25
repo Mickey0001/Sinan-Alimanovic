1031d80e3912a2d38f1bf18d9c8c400f83a0a3a8
Mirza Sisic <mirzasisic57@gmail.com>
2018-07-25 02:47:29 +0200

    functions

--- functions.php
+++ functions.php
@@ -1,1 +1,16 @@
-<?php require('class-wp-bootstrap-navwalker') ?>
+<?php 
+
+//Register Nav Walker class_alias
+require_once('class-wp-bootstrap-navwalker.php');
+
+//Theme setup
+function sinan_theme_setup()
+{
+  register_nav_menus(array(
+    'primary' => __('Primary Menu')
+  ));
+}
+
+add_action('after_setup_theme', 'sinan_theme_setup');
+
+?>
