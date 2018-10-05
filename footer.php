c98b54efe29ab3b611c063041cd3f5e8b081c252
Mirza Sisic <mirzasisic57@gmail.com>
2018-10-05 14:52:28 +0200

    Update Icons and Links

--- footer.php
+++ footer.php
@@ -15,37 +15,37 @@
 	</div><!-- #content -->
 
 	<div class="footer-wrapper">
-		<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
-			<?php get_sidebar('footer'); ?>
-		<?php endif; ?>
-		
-		<?php $toggle_contact = get_theme_mod('toggle_contact_footer', 1); ?>
-		<?php if ( $toggle_contact ) : ?>
+		<?php if (is_active_sidebar('footer-1')): ?>
+			<?php get_sidebar('footer');?>
+		<?php endif;?>
+
+		<?php $toggle_contact = get_theme_mod('toggle_contact_footer', 1);?>
+		<?php if ($toggle_contact): ?>
 		<div class="footer-info">
 			<div class="container">
-				<?php sinan_footer_branding(); ?>
-				<?php sinan_footer_contact(); ?>
+				<?php sinan_footer_branding();?>
+				<?php sinan_footer_contact();?>
 			</div>
-		
-		</div>
-		<?php endif; ?>
 
-		<footer id="colophon" class="site-footer" role="contentinfo">	
+		</div>
+		<?php endif;?>
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="site-info container">
 				<nav id="footernav" class="footer-navigation" role="navigation">
-					<?php wp_nav_menu( array( 'theme_location' => 'footer', 'depth' => '1', 'menu_id' => 'footer-menu' ) ); ?>
+					<?php wp_nav_menu(array('theme_location' => 'footer', 'depth' => '1', 'menu_id' => 'footer-menu'));?>
 				</nav><!-- #site-navigation -->
 				<div class="site-copyright">
-				<p ><?php bloginfo('description'); ?></p>			
+				<p ><?php bloginfo('description');?></p>
 				</div>
-				<p style="text-align: center;">&copy; <?php echo date('Y'); ?> SinanAlimanovic.com</p>
+				<p style="text-align: center; color:rgb(12,124,192);">&copy; <?php echo date('Y'); ?> SinanAlimanovic.com</p>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div>
 
 </div><!-- #page -->
 
-<?php wp_footer(); ?>
+<?php wp_footer();?>
 
 </body>
 </html>
